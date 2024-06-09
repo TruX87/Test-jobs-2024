378086050652b557b966d36dbb82b438d8b11c38
TruX87 <163743905+TruX87@users.noreply.github.com>
2024-06-09 14:51:03 +0300

    Tic-Tac_Toe

---
+++ tic-tac-toe/src/App.js
@@ -0,0 +1,18 @@
+// import React from 'react';
+// import { Route, Link, Routes } from 'react-router-dom';
+// import StartGame from './pages/StartGame';
+// import Game from './pages/Game';
+// import ScoreBoard from './pages/ScoreBoard';
+// import { GameProvider } from './pages/GameContext';
+import './App.css';
+import TicTacToe from './Components/TicTacToe/TicTacToe';
+
+function App() {
+  return (
+    <div>
+      <TicTacToe />
+    </div>
+  );
+}
+
+export default App;
